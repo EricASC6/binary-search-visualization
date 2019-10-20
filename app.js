d7260ac90eb6ae9fa6a6a5cc680cc77204b090e3
Eric Chen <ericchen9025@gmail.com>
2019-10-20 15:01:27 -0400

    generate-btn working

--- app.js
+++ app.js
@@ -5,16 +5,43 @@ const generateRandomArray = () => {
   let array = [];
   let random = 100;
 
-  let i = 0;
-
-  while (i < length) {
+  for (let i = 0; i < length; i++) {
     let num = Math.floor(Math.random() * random);
     if (!array.includes(num)) {
       array.push(num);
+    } else {
+      array.push(num + 1);
     }
-
-    i++;
   }
 
   return array.sort((a, b) => a - b);
 };
+
+const insertArray = arr => {
+  const array = document.querySelector(".array");
+  array.innerHTML = "";
+
+  arr.forEach(num => {
+    const element = document.createElement("div");
+    const data = document.createElement("p");
+    element.className = "element";
+    data.innerHTML = num;
+    element.appendChild(data);
+
+    array.appendChild(element);
+  });
+
+  return;
+};
+
+window.addEventListener("load", () => {
+  let randomArr = generateRandomArray();
+  insertArray(randomArr);
+});
+
+const generate = document.getElementById("generate");
+
+generate.addEventListener("click", () => {
+  let randomArr = generateRandomArray();
+  insertArray(randomArr);
+});
